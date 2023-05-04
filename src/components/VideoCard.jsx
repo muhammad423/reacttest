cd450254d53d9a8b331fdcd891b27a13a94f8783
muhammad423 <mi1933065@gmail.com>
2023-05-04 18:40:33 +0500

    new changes

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,11 +7,11 @@ import { CheckCircle } from '@mui/icons-material';
 const VideoCard = ({video: {id:{videoId}, snippet} }) => {
     console.log(videoId, snippet)
   return (
-    <Card sx={{width: {md: '270px', xs: '100%'}}}>
+    <Card sx={{width: {md: '270px', xs: 'fit-content'}}}>
       <Link to={videoId}>
        <CardMedia image={snippet?.thumbnails?.high?.url}
        alt={snippet?.title}
-       sx={{width:358, height:180}}/>
+       sx={{ height:180}}/>
        <CardContent sx={{backgroundColor: '#1e1e1e' , height: '106px'}}>
          <Link to={videoId}>
           <Typography variant='subtitle2' fontWeight='bold' color='#FFF'>
